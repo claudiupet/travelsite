456b69353b71e9e2a17173de3e9a454944520a77
claudiupet <58388733+claudiupet@users.noreply.github.com>
2019-12-09 12:24:42 +0000

    Add files via upload

---
+++ search_mob2.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/findtr_mob.css">
+    <title>Search people who travel</title>
+</head>
+
+<body>
+  <div class="fullbody">
+    <header>
+    <h1> Find people who are trevelling </h1>
+    </header>
+    <section class="search">
+      <h3>Search all</h3>
+    </section>
+
+    <! ––   first post from the user -->
+    <section class="trpost1">
+    <div class="tpimg">
+      <img src="wom1.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>Amelia is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from London to Madrid</p>
+      </div>
+      <div class="stcount">
+      <p>4</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div>  
+    </section>
+
+
+    <section class="trpost2">
+    <div class="tpimg">
+      <img src="manb.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>Andy is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from Birmingham to Paris</p>
+      </div>
+      <div class="stcount">
+      <p>7</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div>   
+      </section>
+
+      <section class="trpost3">
+    <div class="tpimg">
+      <img src="man1.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>Martin is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from Harrogate to New York</p>
+      </div>
+      <div class="stcount">
+      <p>2</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div>  
+      </section> 
+
+      <section class="trpost4">
+    <div class="tpimg">
+      <img src="man2.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>Dave is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from Winchester to Barcelona</p>
+      </div>
+      <div class="stcount">
+      <p>7</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div> 
+      </section>  
+
+      <section class="trpost5">
+    <div class="tpimg">
+      <img src="man3.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>Lee is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from Liverpool to London</p>
+      </div>
+      <div class="stcount">
+      <p>5</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div>   
+      </section>
+
+    <section class="trpost6">
+    <div class="tpimg">
+      <img src="man4.jpg" alt="sample image" width="320" height="320">
+      <div class="star">
+          <img src="star.png" alt="Interesting stars received" width="25" height="25">
+      </div>      
+      <div class="text1">
+      <p>John is travelling </p>
+    </div>
+      <div class="text2">
+      <p>from Southampton to Bratislava</p>
+      </div>
+      <div class="stcount">
+      <p>7</p>
+      </div>
+      </div>
+      <div class="awes_btn_pos">
+      <button class="awes_btn">  This is interesting! </button>
+     </div>
+      <div>
+      <input class="comm" type="text" name="comm" id="comm" placeholder="  comment">
+    </div>
+      <div class="vcommpos">
+      <button class="vcomm"> View all comments </button>
+     </div>
+     <div class="starbtn">
+          <img src="star.png" alt="Interesting stars btn" width="25" height="25">
+      </div>   
+    </section>
+
+
+    <footer>
+      <h5>   </h5>
+      <p> Claudiu Petraru | petrica05@yahoo.com </p>
+    </footer>
+  </div>
+  <div class="home_btn_pos">
+      <a href="welcome.php"><button class="home_btn"> Home </button>
+     </div> 
+</body>
+
+</html>
